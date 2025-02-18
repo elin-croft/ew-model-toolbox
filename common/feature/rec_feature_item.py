b67ede1d8aa5a3c09bb63ac1a13911e20328fe90
elin-croft <wangyilin19941@outlook.com>
2025-02-18 23:26:45 +0800

    allow shorter && fix

--- common/feature/rec_feature_item.py
+++ common/feature/rec_feature_item.py
@@ -1,8 +1,22 @@
 import logging
-from typing import List
+from typing import List, Dict
 from collections import OrderedDict
 
 import torch
+import torch.nn.functional as F
+
+from .block_config import BlockConfig
+
+def data_padding_1d(data: torch.Tensor, size, allow_shorter, padding_value):
+    if data.ndim > 1:
+        raise Exception("cannot padding a tensor that has two more dim")
+
+    if allow_shorter and size > data.shape[-1]:
+        padding_size = size - data.shape[-1]
+        F.pad(data, (0, padding_size), mode='constant', value=padding_value)
+        return data
+    else:
+        return data
 
 class FeatureItem:
     def __init__(self,
@@ -13,6 +27,8 @@ class FeatureItem:
     ):
         self.feature_vector = feature_vector # raw feature
         self.ordered_feature = ordered_feature
+        self.ori_feature_vector = None # raw feature
+        self.ori_ordered_feature = None
         self.label = label
         self.is_map = is_map
 
@@ -34,3 +50,20 @@ class FeatureItem:
                 msg = f"this feature item works as vector, feature should be torch.Tensor, but got {type(feature)}"
                 raise ValueError(msg)
             self.feature_vector = feature
+    
+    def fix_data(self, blk_cfgs: Dict[int, BlockConfig]):
+        if not self.is_map:
+            return
+        new_data = OrderedDict()
+        ori_data =  self.feature
+        for k, v in ori_data.items():
+            block_id = int(k)
+            cfg = blk_cfgs.get(block_id, None)
+            if cfg:
+                size = cfg.size
+                allow_shorter = cfg.allow_shorter
+                default_value = cfg.default_value
+                v = data_padding_1d(v, size, allow_shorter, default_value)
+            new_data[k] = v
+        self.feature = new_data
+        self.ori_ordered_feature = ori_data
